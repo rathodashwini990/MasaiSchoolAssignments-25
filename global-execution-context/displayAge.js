5e1248ba098fd2bc9637e87a3341ab7da1aa4330
rathodashwini990 <rathodashwini990@gmail.com>
2025-07-24 23:56:55 +0530

    create display age global execution context

---
+++ global-execution-context/displayAge.js
@@ -0,0 +1,14 @@
+var age = 25;
+
+function displayAge (){
+    console.log("Current Age:", age);
+}
+
+function changeAge() {
+    age =30;
+    console.log("Age after update:",age);
+}
+
+displayAge();
+displayAge();
+displayAge();
