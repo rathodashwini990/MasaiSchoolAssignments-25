2968435d230439801d54acf4355ed95073571ebe
rathodashwini990 <rathodashwini990@gmail.com>
2025-07-26 16:08:41 +0530

    Deep vs Shallow copy

---
+++ deep-vs-shallow-copy/deepClone.js
@@ -0,0 +1,15 @@
+function deepClone(obj) {
+  return JSON.parse(JSON.stringify(obj));
+}
+
+let original = {
+  name: "Alice",
+  hobbies: ["reading", "traveling"]
+};
+
+let clone = deepClone(original);
+
+clone.hobbies.push("coding");
+
+console.log("Original:", original);
+console.log("Clone:", clone);
